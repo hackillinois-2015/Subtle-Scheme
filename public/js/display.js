3738d9c3b7912aab235f0a5cd428d4adb87ac842
Jason Lin <j@sonl.in>
2015-02-28 01:48:29 -0600

    progress

--- public/js/display.js
+++ public/js/display.js
@@ -1,7 +1,12 @@
 (function () {
 	var server = io.connect('http://localhost');
-
+	var session = {};//session retrieved from server
 	server.on('connect', function (data) {
 		server.emit('displayJoin');
+	});
+
+	server.on('session update', function (data) {
+		session = JSON.parse(data);
+		console.log(session);
 	});
 }());
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 //mongodb
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/familyFeud');
+mongoose.connect('mongodb://localhost/subtlePineapple');
 //app
 var pdir = __dirname + '/public';
 app.use(express.static(pdir));
@@ -30,12 +30,19 @@ app.get('/display', function (request, response) {
 app.get('/gamepad', function (request, response) {
 	response.render('pages/gamepad');
 });
+
+app.get('/questionSets', function(request, response) {
+	questionSets.find({}, function (error, documents) {
+		response.json(documents);
+	})
+})
 /*******************
 	Variables
 ********************/
 var sessions = [];
 var sessionTemplate = {
 	gameCode: "",
+	clientCount: 0,
 	questionPacks: [],//question pack id's
 	players: [],//array of player objects
 	questionsAsked: [],//question id's
@@ -45,22 +52,91 @@ var playerTemplate = {
 	name: "",
 	score: "",
 };
+var questionSetTemplate = {
+	name: "",
+	questions: []//array of questions
+}
+var questionTemplate = {
+	prompt: "",
+	answer: ""
+}
+/*******************
+	SCHEMAS
+********************/
+var Schema = mongoose.Schema;
+var questionSetSchema = new Schema({
+	name: String,
+	questions: [
+		{
+			prompt: String,
+			answer: String
+		}
+	]
+});
+var questionSets = mongoose.model('question_sets', questionSetSchema);
 /*******************
 	Socket
 ********************/
 io.on('connection', function (socket) {
 	console.log('Device Connected');
+	socket.on('disconnect', function() {
+		console.log("Client Disconnected");
+		//remove socket from room
+		socket.leave(socket.gameCode);
+		sessions[socket.gameCode].clientCount--;
+		//check if the room is abandoned
+		if(sessions[socket.gameCode].clientCount == 0) delete sessions[socket.gameCode];
+	});
 	/*******************
 		Display Specific
 	********************/
 	socket.on('displayJoin', function () {
-		console.log('Display Detected');
+		//generate the code
+		var gameCode = generateGameCode();
+		socket.gameCode = gameCode;
+		//create the session
+		sessions[gameCode] = sessionTemplate;
+		sessions[gameCode].gameCode = gameCode;
+		//add as a client
+		addClient(socket);
+		console.log('New Display: '+socket.gameCode);
 	});
 	/*******************
 		Gamepad Specific
 	********************/
+	socket.on('gamepadJoin', function (gameCode) {
+		//check if gameCode is it's a valid gameCode
+			//if not, emit bad game code
+		//set gameCode
+		socket.gameCode = gameCode;
+		//add client
+		addClient(socket);
+	});
 });
 
+var updateClientSessions = function (gameCode) {
+	io.to(gameCode).emit('session update', JSON.stringify(sessions[gameCode]));
+}
+
+var addClient = function (socket) {
+	socket.join(socket.gameCode);
+	sessions[socket.gameCode].clientCount++;
+	updateClientSessions(socket.gameCode);
+};
+
+var generateGameCode = function () {
+	var uniqueCode = false;
+	while(!uniqueCode) {
+		var gameCode = genRandLetter()+genRandLetter()+genRandLetter()+genRandLetter();
+		uniqueCode = true;
+	}
+	return gameCode;
+};
+
+var genRandLetter = function () {
+	var randInt = Math.floor(Math.random()*26);
+	return String.fromCharCode(65+randInt);
+};
 /*******************
 	Execution
 ********************/
